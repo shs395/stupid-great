24ac7d8169b28345030e36c5424f3ceabdf26ed3
sh0114 <aiis7905@hanmail.net>
2018-11-18 01:53:37 +0900

    살까말까 랜덤 view 완성, 게시물 목록보기 추가중

--- src/components/StupidGreatRandom.vue
+++ src/components/StupidGreatRandom.vue
@@ -1,23 +1,42 @@
 <template>
     <div id="random">
-        
-            <v-card id="random-content"></v-card>
+       <v-card color="transparent">
+              <v-layout>
+                <v-flex xs5>
+                  <v-img
+                    src="https://cdn.pixabay.com/photo/2017/03/27/14/53/macaroons-2179198_1280.jpg"
+                    height="400px"
+                    contain
+                    id="random-img"
+                  ></v-img>
+                </v-flex>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div id="random-content">
+                      <div id="random-head">제목 : <span>마카롱</span></div>
+                      <v-divider light></v-divider><br>
+                      <div>내용 : <span>마카롱 먹고싶은데 살까요 말까요</span></div><br>
+                      <v-divider light></v-divider>
+                      <div id="random-price">가격 : <span>2000원</span></div>
+                    </div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+              <v-card-actions class="pa-3">
+                   <center id="center">
+       <v-btn class="sg-random-btns" id="stupid-btn" color="red">스튜핏!</v-btn>
+                        <v-btn class="sg-random-btns" id="skip-btn" color="grey darken-4">SKIP</v-btn>
+                        <v-btn class="sg-random-btns" id="great-btn" color="blue">그레잇!</v-btn>
 
 
-            <center id="center">
-                <v-btn-toggle max id="sg-random-btn-group">
-                <v-btn class="sg-random-btns" id="stupid-btn" color="red">스튜핏!</v-btn>
-                <v-btn class="sg-random-btns" id="skip-btn" color="grey darken-4">SKIP</v-btn>
-                <v-btn class="sg-random-btns" id="great-btn" color="blue">그레잇!</v-btn>
-                </v-btn-toggle>
+                    <v-btn fab dark bottom right large :direction="direction" color="indigo lighten-2" id="add-btn">
+                        <v-icon>edit</v-icon>
+                    </v-btn>
+                </center>
+              </v-card-actions>
+            </v-card>
 
-                <v-btn fab dark bottom right large :direction="direction" color="indigo lighten-2" id="add-btn">
-                    <v-icon>edit</v-icon>
-                </v-btn>
-            
-            </center>
-            
-    
     </div>
 </template>
 
@@ -31,25 +50,44 @@ export default {
 </script>
 
 <style>
+#random{
+    background: url(../assets/question-bg.jpg) no-repeat center center fixed;
+    -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
 
+  min-block-size: 642px;
+}
+
+#random-img{
+    margin-top:20px;
+    margin-left:173px;
+}
 
 #random-content{
-    width: 400px;
-    height: 370px;
-    margin-top: 30px;
-    margin-bottom: 30px;
-    margin-left: 195px;
+    margin-top:50px;
+    margin-left: 50px;
+    font-size:30px;
+    color:#FFF8E1;
+}
+
+#random-price{
+    color:#EF5350;
+}
+
+#random-head{
+    font-size:50px;
 }
 
 #center{
-    margin-left:195px;
+    margin-left:157px;
 }
 
 .sg-random-btns{
-    width: 600px;
+    width: 373px;
     height:150px;
-    font-size: 80px;
-    margin: auto 0;
+    font-size: 70px;
     font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 }
 
@@ -79,6 +117,5 @@ export default {
 #add-btn{
     margin-top:60px;
     margin-left: 120px;
-
 }
 </style>
--- src/views/StupidGreatCommunity.vue
+++ src/views/StupidGreatCommunity.vue
@@ -2,15 +2,25 @@
     <div id="sg-community-bg">
     <toolbar />
     <v-tabs fixed-tabs>
-        <v-tab
-        v-for="str in strs"
-        :key="str"
-        >
-        {{ str }}
+        <v-tab>
+            랜덤으로 보기
         </v-tab>
-    </v-tabs>
+        <v-tab>
+            게시물 목록 보기
+        </v-tab>
 
-    <StupidGreatRandom />
+        <v-tab-item>
+            <v-card>
+                    <StupidGreatRandom />
+            </v-card>
+        </v-tab-item>
+
+        <v-tab-item>
+            <v-card>
+                <h1>게시물목록</h1>
+            </v-card>
+        </v-tab-item>
+    </v-tabs>
     </div>
 </template>
 
@@ -26,7 +36,6 @@ export default {
     },
     data (){
         return {
-            strs : ['랜덤으로 보기', '게시물 목록 보기'],
 
         }
     }
