13bec5f87b4570cc81fb500ee9e0ffa65bc11931
Aerim <wjsdofla7882@ajou.ac.kr>
2018-12-02 04:04:23 +0900

    board db수정, 게시판 볼 수 있게 수정

--- backend/routes/board.js
+++ backend/routes/board.js
@@ -3,29 +3,42 @@ var router = express.Router();
 const bodyParser = require('body-parser')
 // const userModel = require('../db/models/user')
 const accountModel = require('../db/models/account')
-const writerModel = require('../db/models/writer')
+const boardModel = require('../db/models/board')
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json())
 
 
+//게시글 작성
 router.post('/post',(req,res,next)=>{
-   var writer=new writerModel({writer:req.body.id,title:req.body.title,content:req.body.body,date:req.body.date})
+   var boardPost=new boardModel({writer:req.body.id,title:req.body.title,content:req.body.body,createdAt:req.body.createdAt})
 
-    writer.save(function(err){
+    boardPost.save(function(err){
         if(err) {(console.log(err))}
 
-        console.log('hi')
-        res.send(writer)
+        console.log('Saved!')
+        res.send(boardPost)
     })
 })
 
+//게시글 목록보기
 router.get('/list',(req,res,next)=>{
-    writerModel.find({},function(err,writers){
+    boardModel.find({},function(err,boards){
         if(err) {console.log(err)}
 
-        res.send(writers)
+        res.send(boards)
     })
+})
+
+//게시글 내용보기
+router.get('/read/:id/:postNumber',async(req,res,next)=>{
+    const id=req.param.id;
+    const postNumber=req.params.postNumber;
+
+    var boardFind=await boardModel.find({})
+    
+    console.log(boardFind)
+    res.send(boardFind)
 })
 
    
