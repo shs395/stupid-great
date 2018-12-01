b1c990f771877d3a76480bb571a233da8edd5585
shs395@ajou.ac.kr <shs395@ajou.ac.kr>
2018-12-01 12:09:07 +0900

    12시

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,8 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const accountRouter = require('./routes/account')
 const statsRouter = require('./routes/stats')
+const sgRouter = require('./routes/stupid_great');
+const boardRouter=require('./routes/board')
 
 const app = express();
 
@@ -34,12 +36,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
-
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const accountRouter = require('./routes/account')
-const sgRouter = require('./routes/stupid_great');
-const boardRouter=require('./routes/board')
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
