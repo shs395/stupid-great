5a22e6899bcc8cd4b215445fdc74c893d86c7cbc
shs395@ajou.ac.kr <shs395@ajou.ac.kr>
2018-12-08 15:45:52 +0900

    조건에 안맞으면 내 데이터 안보이는 것 수정

--- backend/routes/stats.js
+++ backend/routes/stats.js
@@ -9,8 +9,25 @@ router.post('/find',async (req,res)=>{
   res.send(user)
 })
 
-router.post('/conditional-search',async(req,res)=>{
-  console.log("---------- 조건검색에서 조건 ----------")
+router.post('/me-search',async(req,res)=>{
+  console.log("내 것만 검색하기")
+  console.log(req.body.year)
+  console.log(req.body.month)
+  console.log(req.body.id)
+  var find = await accountModel.find({
+    $and : [
+      {year : req.body.year},
+      {month : req.body.month},
+      {id : req.body.id}
+    ]
+  })
+  console.log(find)
+  console.log("good")
+  res.send(find)
+})
+
+router.post('/others-search',async(req,res)=>{
+  console.log("---------- 조건검색에서 조건 (나 빼고 찾기) ----------")
   console.log(req.body)
   var startAge = req.body.startAge
   var endAge = req.body.endAge
@@ -22,13 +39,14 @@ router.post('/conditional-search',async(req,res)=>{
         {age : {$gte:startAge,$lte:endAge}}
       ]},
       {year : req.body.year},
-      {month : req.body.month}
+      {month : req.body.month},
+      {id :{$ne:req.body.id}}
     ]
   })
   console.log(find)
   console.log("*********find*********")
 
-
+//일치하는 사람 id 배열에 담음 (나 빼고)
   var personCount = await accountModel.distinct("id",{
     $and : [
       {job : { $in : req.body.job}},
@@ -37,14 +55,15 @@ router.post('/conditional-search',async(req,res)=>{
         {age : {$gte:startAge,$lte:endAge}}
       ]},
       {year : req.body.year},
-      {month : req.body.month}
+      {month : req.body.month},
+      {id :{$ne:req.body.id}}
     ]
   });
+  console.log(personCount.length)
   console.log(personCount)
   find.push(personCount)
   console.log("*********personCount*********")
-
-
+  console.log(find)
 
   res.send(find)
 })
