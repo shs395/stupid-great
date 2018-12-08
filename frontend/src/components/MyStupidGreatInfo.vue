bb599cd4486104851c2b58437a2440f29c2e381f
sh0114 <aiis7905@hanmail.net>
2018-12-08 16:02:14 +0900

    마이페이지 stugpid great 결과 보는거 추가

---
+++ frontend/src/components/MyStupidGreatInfo.vue
@@ -0,0 +1,43 @@
+<template>
+    <div id="MyStupidGreatInfo">
+        <v-card>
+            <v-layout>
+                <v-flex xs5>
+                    <v-img
+                        :src="imgpath"
+                        height="170px"
+                        contain
+                    ></v-img>
+                </v-flex>
+                
+                <v-flex xs7>
+                    <v-card-title primary-title>
+                        <div>
+                        <div class="headline">제목 : <span></span></div>
+                        <div>내용 : </div>
+                        <div>가격 : </div>
+                        </div>
+                    </v-card-title>
+                    </v-flex>
+            </v-layout>
+                
+            <v-divider light></v-divider>
+            
+            <v-card-actions>
+                <h3>stupid? great! 결과 보여주기</h3>
+            </v-card-actions>
+            
+        </v-card>
+    </div>
+</template>
+
+<script>
+
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
