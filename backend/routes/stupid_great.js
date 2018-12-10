baf18b6f46d4fc7453d09daa4b22994abb101402
Eumji <jihye97@ajou.ac.kr>
2018-12-11 01:32:52 +0900

    image

--- backend/routes/stupid_great.js
+++ backend/routes/stupid_great.js
@@ -93,8 +93,15 @@ const upload = multer({
     }),
 });
 
-router.post('/create/img',upload.single('img'),function(req, res){
-    res.json({"state": "ok", "imgname":imgname});
+router.post('/create/img',function(req, res){
+    upload.single('img')
+    .then((result)=>{
+        console.log(result)
+        res.json({"state": "ok", "imgname":imgname});
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
 });
 
 /*-------------------------------------Stupid great result page----------------------------------------------*/
