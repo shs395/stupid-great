938d088e656d5802ade68fcfc3a871764646603b
root <root@instance-1.asia-northeast1-c.c.army-252301.internal>
2019-11-29 10:24:47 +0000

    minor bug fix

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -108,13 +108,13 @@
       return {
         items: [
           {
-            src: 'http://52.79.192.162:3000/static/img/money1.jpg'
+            src: '/static/img/money1.jpg'
           },
           {
-            src: 'http://52.79.192.162:3000/static/img/money2.jpg'
+            src: '/static/img/money2.jpg'
           },
           {
-            src: 'http://52.79.192.162:3000/static/img/money3.jpg'
+            src: '/static/img/money3.jpg'
           }
         ]
       }
